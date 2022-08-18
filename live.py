a2982d9b64442de2ae7c00453c580dbb1748c4d0
abdoShahat <35191025+abdoShahat@users.noreply.github.com>
2022-08-18 12:03:26 +0200

    Update live.py

--- live.py
+++ live.py
@@ -104,7 +104,7 @@ def shadow_mask(src: np.ndarray, points: np.ndarray, color: list):
 # )
 
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    {"iceServers": [{"urls": ["stun.12connect.com:3478"]}]}
 )
 
 webrtc_ctx = webrtc_streamer(
